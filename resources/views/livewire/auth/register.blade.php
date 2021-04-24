7a443cf76a6754c1881c4e822733014bc3eee120
FabioPerez <fanpero87@gmail.com>
2021-04-24 18:14:47 +0000

    first commit

---
+++ resources/views/livewire/auth/register.blade.php
@@ -0,0 +1,25 @@
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">name</label>
+        <input wire:model="name" type="text" id="name" name="name">
+        @error('name') <span> {{ $message }}</span> @enderror
+    </div>
+    <div>
+        <label for="email">email</label>
+        <input wire:model="email" type="text" id="email" name="email">
+        @error('email') <span> {{ $message }}</span> @enderror
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input wire:model="password" type="password" id="password" name="password">
+        @error('password') <span> {{ $message }}</span> @enderror
+    </div>
+    <div>
+        <label for="passwordConfirmation">passwordConfirmation</label>
+        <input wire:model="passwordConfirmation" type="password" id="passwordConfirmation" name="passwordConfirmation">
+        @error('passwordConfirmation') <span> {{ $message }}</span> @enderror
+    </div>
+    <div>
+        <input type="submit" value="Register">
+    </div>
+</form>
---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Livewire\Livewire;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function can_register()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio@example.com')
+            ->set('password', 'password')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertRedirect('/');
+
+        $this->assertTrue(User::whereEmail('fabio@example.com')->exists());
+        $this->assertEquals('fabio@example.com', auth()->user()->email);
+    }
+
+    /** @test */
+    function email_is_require()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', '')
+            ->set('password', 'password')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertHasErrors(['email' => 'required']);
+    }
+
+    /** @test */
+    function email_is_valid_email()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio')
+            ->set('password', 'password')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertHasErrors(['email' => 'email']);
+    }
+
+    /** @test */
+    function email_hasnt_been_taken_already()
+    {
+        User::create([
+            'name' => 'fabio',
+            'email' => 'fabio@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio@example.com')
+            ->set('password', 'password')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertHasErrors(['email' => 'unique']);
+    }
+
+    /** @test */
+    function password_is_required()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio@example.com')
+            ->set('password', '')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertHasErrors(['password' => 'required']);
+    }
+
+    /** @test */
+    function password_is_minimum_of_six_characters()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio@example.com')
+            ->set('password', 'fab')
+            ->set('passwordConfirmation', 'password')
+            ->call('register')
+            ->assertHasErrors(['password' => 'min']);
+    }
+
+    /** @test */
+    function password_matches_password_confirmation()
+    {
+        Livewire::test('auth.register')
+            ->set('name', 'fabio')
+            ->set('email', 'fabio@example.com')
+            ->set('password', 'password')
+            ->set('passwordConfirmation', 'non-password')
+            ->call('register')
+            ->assertHasErrors(['password' => 'same']);
+    }
+}
